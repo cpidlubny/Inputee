1d2fde999eba85401d4017bef69269b3f78c995e
clint <clint@pidlubny.com>
2013-07-31 07:33:36 -0500

    added new question functionality

--- app/controllers/ask_controller.rb
+++ app/controllers/ask_controller.rb
@@ -1,5 +1,11 @@
 class AskController < ApplicationController
+  
   def new
+    @question = Question.new(params[:question])
+    if request.post? and @question.save
+      flash.now[:notice] = "Question submitted"
+      @question = Question.new
+    end
   end
 
   def list
